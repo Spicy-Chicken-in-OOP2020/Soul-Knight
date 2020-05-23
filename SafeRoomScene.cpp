99256455d1aed8603e46582db4a8ce77a8cd7a5c
JunjieLl <1061455426@qq.com>
2020-05-23 16:06:16 +0800

    加载了暂停界面，对继承关系有所改变，层次更清晰

--- SafeRoomScene.cpp
+++ SafeRoomScene.cpp
@@ -1,58 +1,87 @@
 #include "SafeRoomScene.h"
 #include "Hero.h"
 #include "MoveController.h"
+#include "ShootController.h"
 #include "editor-support/cocostudio/CCSGUIReader.h"
 #include "ui/CocosGUI.h"
 #include "MonsterManager.h"
+#include "PauseLayer1.h"
 USING_NS_CC;
+
 using namespace cocos2d::ui;
 using namespace cocostudio;
+MonsterManager* SafeRoomScene::_monsterManager = nullptr;
 
+PauseLayer* SafeRoomScene::_pauseLayer = nullptr;
+
+SafeRoomScene* SafeRoomScene::_safeRoomScene = nullptr;
 
 Scene* SafeRoomScene::createScene() {
-	auto scene = Scene::create();
-	auto layer1 = SafeRoomScene::create();
-	scene->addChild(layer1);
+	/*创建场景*/
+	if (_safeRoomScene != nullptr)
+		return _safeRoomScene;
 
-	auto layer2=layer1->mainUiInit(layer1->_hero);
-	scene->addChild(layer2);
+	_safeRoomScene = SafeRoomScene::create();
+	_safeRoomScene->retain();
+	
+	/*添加地图*/
+	_safeRoomScene->addMap();
 
-	return scene;
+	/*左上角*/
+	auto layer = _safeRoomScene->mainUiInit(_safeRoomScene->_hero);
+	_safeRoomScene->addChild(layer,2);
+
+	/*暂停界面*/
+	_pauseLayer = PauseLayer::createPauseLayer();
+	_pauseLayer->setVisible(false);
+	_safeRoomScene->addChild(_pauseLayer, 3);
+
+	return _safeRoomScene;
 }
 
 bool SafeRoomScene::init() {
-	if (!Layer::init()) {
+	if (!Scene::init()) {
 		return false;
 	}
-	_hero = nullptr;
-	TMXTiledMap* safeRoomMap = TMXTiledMap::create("MainMap.tmx");
-	this->addChild(safeRoomMap);
-	//add hero
-	_hero = addHero(safeRoomMap);
-
-	//test monster
-	//auto monsterMgr = MonsterManager::createMonsterManagerWithLevel(1);
-	//this->addChild(monsterMgr);
 	return true;
 }
 
-Hero* SafeRoomScene::addHero(TMXTiledMap* map) {
+void SafeRoomScene::addMap() {
+	auto layer = Layer::create();
+	_hero = nullptr;
+	TMXTiledMap* safeRoomMap = TMXTiledMap::create("MainMap.tmx");
+	//设定当前所在地图
+	GlobalParameter::mapNow = safeRoomMap;
+	layer->addChild(safeRoomMap);
+	//add hero
+	_hero =addHero(safeRoomMap, layer);
+
+	//test monster
+	_monsterManager = MonsterManager::createMonsterManagerWithLevel(1);
+	layer->addChild(_monsterManager);
+
+	_safeRoomScene->addChild(layer, 1);
+}
+
+Hero* SafeRoomScene::addHero(TMXTiledMap* map, Layer* layer) {
 
 	auto visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
 
 	Sprite* playerSprite = Sprite::create("Knight1.png");
 
 	Hero* mPlayer = Hero::create(playerSprite);
 	mPlayer->setPosition(Point(100, visibleSize.height / 2));
-	this->addChild(mPlayer);
+	layer->addChild(mPlayer);
 
 	mPlayer->setSafeRoomTiledMap(map);
 
 	MoveController* move = MoveController::create();
-	this->addChild(move);
-	mPlayer->setController(move);
+	ShootController* shoot = ShootController::create();
+	layer->addChild(move);
+	
+	
+	mPlayer->setController(move,shoot);
 
 	TMXObjectGroup* heroGroup = map->getObjectGroup("hero");
 	ValueMap heroPointMap = heroGroup->getObject("heroDir");
@@ -65,13 +94,27 @@ Hero* SafeRoomScene::addHero(TMXTiledMap* map) {
 	return mPlayer;
 }
 
-Layer* SafeRoomScene::mainUiInit(Hero* hero) {
+void SafeRoomScene::pauseButton(Ref*, TouchEventType type) {
+	switch (type) {
+	case TouchEventType::TOUCH_EVENT_ENDED: {
+		_pauseLayer->setVisible(true);
+		break;
+	}
+	}
+}
+
+Layer* SafeRoomScene::mainUiInit(Hero* hero){
+
 	auto layer = Layer::create();
 	auto mainUI = GUIReader::getInstance()->widgetFromJsonFile("MainUI.ExportJson");
 	layer->addChild(mainUI);
-	Button* stopButton = (Button*)Helper::seekWidgetByName(mainUI, "stopButton");
+	Button* stopButton = (Button*)Helper::seekWidgetByName(mainUI, "StopButton");
 
+	/*暂停界面调取*/
+	stopButton->addTouchEventListener(stopButton, toucheventselector(SafeRoomScene::pauseButton));
+	
 	//获取主角HP,护甲,MP
+
 	auto playerHpUI = (LoadingBar*)Helper::seekWidgetByName(mainUI, "PlayerHpUI");
 	auto playerHpLabel = (Label*)Helper::seekWidgetByName(mainUI, "BoundaryHp");
 	auto playerHpMaxLabel = (Label*)Helper::seekWidgetByName(mainUI, "BoundaryHpMax");
--- SafeRoomScene.h
+++ SafeRoomScene.h
@@ -1,22 +1,40 @@
 #ifndef __SAFEROOMSCENE_H__
 #define __SAFEROOMSCENE_H__
 
-#include "cocos2d.h"
 #include "Hero.h"
-USING_NS_CC;
+#include "MonsterManager.h"
+#include "ShootController.h"
+#include "GlobalParameter.h"
+#include "PauseLayer.h"
 
-class SafeRoomScene :public Layer {
+class SafeRoomScene :public Scene {
 public:
 	CREATE_FUNC(SafeRoomScene);
 	virtual bool init();
 	static Scene* createScene();
-	Hero* addHero(TMXTiledMap* map);
+	Hero* addHero(TMXTiledMap* map,Layer* layer);
 
+	/*load UI layer*/
 	Layer* mainUiInit(Hero* hero);
+	/*添加地图层*/
+	void addMap();
+	/*暂停按钮回调*/
+	void pauseButton(Ref*, TouchEventType type);
 
 private:
 
+	/*英雄*/
 	Hero* _hero;
+	/*暂停界面*/
+	static PauseLayer* _pauseLayer;
+	/*场景*/
+	static SafeRoomScene* _safeRoomScene;
+
+public:
+	/*怪物管理器*/
+	static MonsterManager* _monsterManager;
+
+
 };
 
 #endif
