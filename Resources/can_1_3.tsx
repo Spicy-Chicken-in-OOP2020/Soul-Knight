c0ca08cfc1548feba420d8a1218e0454b50ef02e
wangwangwang2333 <1209497940@qq.com>
2020-06-01 18:49:40 +0800

    增加宝箱开闭图

--- Resources/can_1_3.tsx
+++ Resources/can_1_3.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.4" name="can" tilewidth="48" tileheight="48" tilecount="4" columns="4">
- <image source="../picture/can.png" width="192" height="48"/>
+ <image source="can.png" width="192" height="48"/>
  <tile id="0">
   <properties>
    <property name="Hot" type="bool" value="true"/>
